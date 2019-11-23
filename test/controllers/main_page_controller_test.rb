c48f01589afca2088233961bcbc2eacf32cbf91b
geraldgsh <geraldgsh@hotmail.com>
2019-11-24 02:13:20 +0800

    test for main page controller

--- test/controllers/main_page_controller_test.rb
+++ test/controllers/main_page_controller_test.rb
@@ -1,14 +1,20 @@
 require 'test_helper'
 
 class MainPageControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Event Planning APP"
+  end
+
   test "should get home" do
     get main_page_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get main_page_about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
